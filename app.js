d7221262298ff822213915cf61448c0e0c470e0b
jopbdavid <jopbdavid@gmail.com>
2023-06-23 09:40:27 +0100

    done controllers with stats

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//rate limiter for deployment
+app.set("trust proxy", 1);
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
 app.use(helmet());
